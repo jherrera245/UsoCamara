d36871b659d38e3c8d42dee4a95270e9aef7eba4
jherrera245 <245jh17@gmail.com>
2023-02-18 15:20:44 -0600

    Creación de proyecto y adicción de permisios

---
+++ app/src/main/java/com/jherrera/usocamara/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jherrera.usocamara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
